0b06f716c87cbe92c10c6ae3959acf981c2ee58d
shd188 <shd_di@163.com>
2015-06-23 23:17:36 +0800

    commit hack08

--- app/src/main/java/com/aimer/shd188/hack08/MainActivity.java
+++ app/src/main/java/com/aimer/shd188/hack08/MainActivity.java
@@ -1,38 +1,116 @@
 package com.aimer.shd188.hack08;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+import com.nineoldandroids.animation.Animator;
+import com.nineoldandroids.animation.Animator.AnimatorListener;
+import com.nineoldandroids.animation.AnimatorSet;
+import com.nineoldandroids.animation.ObjectAnimator;
+import com.nineoldandroids.view.animation.AnimatorProxy;
+
+import java.util.Random;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity implements AnimatorListener {
+
+    private FrameLayout mContainer;
+    private ImageView mView;
+    private Random mRandom = new Random();
+    private int mIndex = 0;
+
+    private static final int[] PHOTOS = new int[]{R.drawable.photo1, R.drawable.photo2, R.drawable.photo3, R.drawable.photo4, R.drawable.photo5, R.drawable.photo6};
+    private static final int ANIM_COUNT = 4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        //创建布局容器
+        mContainer = new FrameLayout(this);
+        mContainer.setLayoutParams(new WindowManager.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        //创建ImageView，并将其添加到布局容器中
+        mView = createNewView();
+        mContainer.addView(mView);
+
+        setContentView(mContainer);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    protected void onResume() {
+        super.onResume();
+        nextAnimation();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    private ImageView createNewView() {
+        ImageView ret = new ImageView(this);
+        ret.setLayoutParams(new WindowManager.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        ret.setScaleType(ImageView.ScaleType.FIT_XY);
+        ret.setImageResource(PHOTOS[mIndex]);
+        mIndex = (mIndex + 1 < PHOTOS.length) ? mIndex + 1 : 0;
+        return ret;
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    private void nextAnimation() {
+        AnimatorSet anim = new AnimatorSet();
+
+        final int index = mRandom.nextInt(ANIM_COUNT);
+
+        switch (index) {
+            case 0:
+                anim.playTogether(ObjectAnimator.ofFloat(mView, "scaleX", 1.5f, 1f), ObjectAnimator.ofFloat(mView, "scaleY", 1.5f, 1f));
+                break;
+            case 1:
+                anim.playTogether(
+                        ObjectAnimator.ofFloat(mView, "scaleX", 1, 1.5f),
+                        ObjectAnimator.ofFloat(mView, "scaleY", 1, 1.5f));
+                break;
+
+            case 2:
+                AnimatorProxy.wrap(mView).setScaleX(1.5f);
+                AnimatorProxy.wrap(mView).setScaleY(1.5f);
+                anim.playTogether(ObjectAnimator.ofFloat(mView,
+                        "translationY", 80f, 0f));
+                break;
+
+            case 3:
+            default:
+                AnimatorProxy.wrap(mView).setScaleX(1.5f);
+                AnimatorProxy.wrap(mView).setScaleY(1.5f);
+                anim.playTogether(ObjectAnimator.ofFloat(mView,
+                        "translationX", 0f, 40f));
+                break;
         }
+        anim.setDuration(3000);
+        anim.addListener(this);
+        anim.start();
+    }
 
-        return super.onOptionsItemSelected(item);
+    @Override
+    public void onAnimationStart(Animator animator) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animator animator) {
+        mContainer.removeView(mView);
+        mView = createNewView();
+        mContainer.addView(mView);
+
+        nextAnimation();
+    }
+
+    @Override
+    public void onAnimationCancel(Animator animator) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animator animator) {
+
     }
 }
